a0c3f76fb0db61368cbe0da005e16450a4f95bd0
Edd Steel <edward.steel@hootsuite.com>
2013-01-04 10:32:38 -0800

    update

--- bin/tmux_windowtoggle.sh
+++ bin/tmux_windowtoggle.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env zsh
-
-. ~/.zshrc
+. ~/.zshfunctions/tmux
 
 local=$(hostname -s)
 name=$(get_tmux_wvar "hostname" || echo $local)
 
 current=$(get_tmux_wvar "synced")
-echo $current
 
 if [ "$current" = "true" ]; then
-	tmux setw window-status-current-fg colour033
+	tmux setw window-status-current-fg colour033 >/dev/null
 	set_tmux_wvar "synced" "false"
 else
-	tmux setw window-status-current-fg white
+	tmux setw window-status-current-fg white >/dev/null
 	set_tmux_wvar "synced" "true"
 fi
