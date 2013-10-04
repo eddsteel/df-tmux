cc10c2946a810c9e42aaf6288af8019a0de78029
Edd Steel <edward.steel@hootsuite.com>
2013-10-03 17:00:32 -0700

    update

--- Projects/scripts/vantemp.sh
+++ Projects/scripts/vantemp.sh
@@ -11,13 +11,17 @@ if [ -f ~/.lasttemp ]; then
 	thn=$(date -j -f '%s' "$latest" +'%s')
 	now=$(date +'%s')
 	threshold=600
-	diff=$(echo "$thn - $now" | bc)
+	diff=$(expr $now - $thn)
+
+	echo "$diff <> $threshold" >&2
 
 	if [ $diff -gt $threshold ]; then check=true; else check=false; fi
 fi
 
 if $check; then
-	temp=$(curl http://weather.noaa.gov/weather/current/CYVR.html 2>/dev/null | grep Temperature -A2 -m1 | tail -n 1 | sed -e 's/.*(\(.*\)).*/\1/' -e 's/ /°/' | tr -d '[:space:]')
+	temp=$(curl http://weather.noaa.gov/weather/current/CYVR.html 2>/dev/null | \
+		grep Temperature -A2 -m2 | tail -n 3 | head -n 1 | \
+		sed -e 's/.*(\(.*\)).*/\1/' -e 's/ /°/' | tr -d '[:space:]')
 
 	echo "$(date +'%s')@$temp" > ~/.lasttemp
 	echo $temp
