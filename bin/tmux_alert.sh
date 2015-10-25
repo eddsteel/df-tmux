f007182fefdafc7cf934754b9041e6776cfc4a3c
Edd Steel <edward.steel@hootsuite.com>
2015-10-24 23:30:40 -0700

    streamline alert

--- bin/tmux_alert.sh
+++ bin/tmux_alert.sh
@@ -5,9 +5,13 @@ grey=colour234
 fg=colour234
 
 if tmux list-windows -F"#{window_flags}" | grep '!'>/dev/null; then
-	bg=colour124
-	fg=colour252
+        bg=colour124
+        fg=colour252
+        go=true
 fi
 
-echo "#[fg=$grey,bold]#[bg=$bg]⮀#[bg=$bg]#[fg=$fg]!#[bg=$grey]#[fg=$bg]⮀"
-
+if [ $go ]; then
+    echo "#[fg=$grey,bold]#[bg=$bg]⮀#[bg=$bg]#[fg=$fg]!#[bg=$grey]#[fg=$bg]⮀"
+else
+    echo ""
+fi
