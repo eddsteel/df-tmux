3b60d8d57bd64be48f204a58fb21d2d06542dcb1
Edd Steel <edward.steel@hootsuite.com>
2015-10-24 23:30:28 -0700

    simplify/move some scripts

--- bin/tmux_ssh_split.sh
+++ bin/tmux_ssh_split.sh
@@ -3,28 +3,15 @@
 # tmux_ssh_split.sh - splits current window into ssh sessions to provided
 # servers.
 #
-# usage e.g. tmux_ssh_split.sh server{1..3}.domain.com
-#            tmux_ssh_split.sh server-group (where server-group is a file in
-#            ~/.host-groups containing a space-separated list of hosts.)
 
-if [ $# -gt 1 ]; then
-	boxes=$*
-	title=multi-ssh
-elif [ $# -eq 1 ]; then
-	boxes=$(cat ~/.host-groups/$1)
-	title=$(echo $1 | sed 's/\..*$//g')
-else
-	echo "usage: $0 [host-group|hostnames...] where host-groups are defined in ~/.host-groups"
-	echo "available host groups:"
-	ls -1 ~/.host-groups
-	exit 1
-fi
+boxes=$*
+title=$(echo $boxes | cut -c 1-12)
 
 tmux neww
 for box in $boxes; do
-	# TODO: pull in hostname setting stuff from ssh()
-	tmux split -h "ssh \"$box\""
-	tmux select-layout tiled>/dev/null
+        # TODO: pull in hostname setting stuff from ssh()
+        tmux split -h "ssh \"$box\""
+        tmux select-layout tiled>/dev/null
 done
 tmux kill-pane -t 0
 tmux select-layout tiled>/dev/null
